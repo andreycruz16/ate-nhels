fcb026fe784214705ed2615881eddc57a87eb15e
Mark Andrey <markandreydelacruz@ymail.com>
2023-03-01 20:29:11 +0800

    added menus

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import './App.css'
+import "./App.css";
+import image1 from "./assets/Ate_Nhels_Page_1.jpg";
+import image2 from "./assets/Ate_Nhels_Page_1.jpg";
+import image3 from "./assets/Ate_Nhels_Page_1.jpg";
+import image4 from "./assets/Ate_Nhels_Page_1.jpg";
 
 function App() {
   return (
     <div className="App">
       <h1>Ate Nhel's</h1>
+      <img src={image1} alt="Ate_Nhels_Page_1" height="962px" width="680px" />
+      <img src={image2} alt="Ate_Nhels_Page_2" height="962px" width="680px" />
+      <img src={image3} alt="Ate_Nhels_Page_3" height="962px" width="680px" />
+      <img src={image4} alt="Ate_Nhels_Page_4" height="962px" width="680px" />
+      <a href="tel:9498701629"><h1>09498701629</h1></a>
+      <h2>Contact Number</h2>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
