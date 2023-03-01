f231b7645dca4fb893e300794378c8f993278bc5
Mark Andrey <markandreydelacruz@ymail.com>
2023-03-01 20:40:05 +0800

    make img responsive

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,10 @@ function App() {
   return (
     <div className="App">
       <h1>Ate Nhel's</h1>
-      <img src={image1} alt="Ate_Nhels_Page_1" height="962px" width="680px" />
-      <img src={image2} alt="Ate_Nhels_Page_2" height="962px" width="680px" />
-      <img src={image3} alt="Ate_Nhels_Page_3" height="962px" width="680px" />
-      <img src={image4} alt="Ate_Nhels_Page_4" height="962px" width="680px" />
+      <img src={image1} alt="Ate_Nhels_Page_1" className="responsive" />
+      <img src={image2} alt="Ate_Nhels_Page_2" className="responsive" />
+      <img src={image3} alt="Ate_Nhels_Page_3" className="responsive" />
+      <img src={image4} alt="Ate_Nhels_Page_4" className="responsive" />
       <a href="tel:9498701629"><h1>09498701629</h1></a>
       <h2>Contact Number</h2>
     </div>
