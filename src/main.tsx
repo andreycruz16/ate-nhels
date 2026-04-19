6cde76a82b246e21dd84e32b530356188b58e0f4
Mark Andrey <markandreydelacruz@ymail.com>
2026-04-19 10:27:11 +0800

    add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import App from './App'
 import { menuContent } from './features/menu/data/menuContent'
 import './index.css'
@@ -9,5 +10,6 @@ document.title = menuContent.browserTitle
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>,
 )
